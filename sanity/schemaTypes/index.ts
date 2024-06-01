e0c9f6c09b618090e42cd04216bf66b65f43dd1d
Mike <michkhol@yahoo.com>
2024-05-31 21:22:41 -0400

    Added sanity and static pages

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import blockContent from './blockContent'
+//import category from './category'
+import post from './richParagraph'
+//import author from './author'
+
+export const schemaTypes = [blockContent, post]
