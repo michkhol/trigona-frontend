fbc0ace97534046a69ec766a9be25593a41a0458
Mike <michkhol@yahoo.com>
2024-09-18 17:33:16 -0400

    Added corse name

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -6,6 +6,7 @@ export default async function Pricing2() {
   const priceList = (await stripe.prices.list()).data
   return (
     <div>
+      <h1 className="text-center text-5xl mb-6"><span className="italic">Practical Change Management, Enhanced with AI</span> <span className="text-red-800 font-semibold">Course</span></h1>
       <PriceList2Register prices={priceList}/>
       <div className="my-8 flex justify-center">
            <Link href="/courses" className="bg-red-800 text-white text-2xl mt-8 px-8 py-3 rounded-full font-bold text-center hover:bg-orange-600 transition-colors w-52">
